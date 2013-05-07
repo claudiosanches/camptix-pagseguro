260faaa2f9fd613dd866b8a0620cae524cd4f487
claudiosmweb <contato@claudiosmweb.com>
2013-05-07 19:05:32 -0300

    added initial files

---
+++ camptix-pagseguro.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Plugin Name: CampTix PagSeguro
+ * Plugin URI: https://github.com/claudiosmweb/camptix-pagseguro
+ * Description: PagSeguro Gateway for CampTix.
+ * Author: claudiosanches
+ * Author URI: http://claudiosmweb.com/
+ * Version: 1.0.0
+ * License: GPLv2 or later
+ * Text Domain: ctpagseguro
+ * Domain Path: /languages/
+ */
