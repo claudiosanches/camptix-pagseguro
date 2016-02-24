97c571aa3b0081c08990c80a0d5115478f0accbd
Claudio Sanches <contato@claudiosmweb.com>
2016-02-23 22:39:09 -0300

    Fixed indentation

--- camptix-pagseguro.php
+++ camptix-pagseguro.php
@@ -17,11 +17,11 @@
  * @return string HTML Message.
  */
 function ctpagseguro_admin_notice() {
-    $html = '<div class="error">';
-        $html .= '<p>' . sprintf( __( 'CampTix PagSeguro Gateway depends on the last version of %s to work!', 'ctpagseguro' ), '<a href="http://wordpress.org/extend/plugins/camptix/">CampTix</a>' ) . '</p>';
-    $html .= '</div>';
+	$html = '<div class="error">';
+		$html .= '<p>' . sprintf( __( 'CampTix PagSeguro Gateway depends on the last version of %s to work!', 'ctpagseguro' ), '<a href="http://wordpress.org/extend/plugins/camptix/">CampTix</a>' ) . '</p>';
+	$html .= '</div>';
 
-    echo $html;
+	echo $html;
 }
 
 /**
@@ -30,14 +30,14 @@ function ctpagseguro_admin_notice() {
  * @return void
  */
 function ctpagseguro_plugins_loaded() {
-    load_plugin_textdomain( 'ctpagseguro', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+	load_plugin_textdomain( 'ctpagseguro', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
 
-    if ( ! class_exists( 'CampTix_Plugin' ) || ! class_exists( 'CampTix_Payment_Method' ) ) {
-        add_action( 'admin_notices', 'ctpagseguro_admin_notice' );
-        return;
-    }
+	if ( ! class_exists( 'CampTix_Plugin' ) || ! class_exists( 'CampTix_Payment_Method' ) ) {
+		add_action( 'admin_notices', 'ctpagseguro_admin_notice' );
+		return;
+	}
 
-    add_action( 'camptix_load_addons', 'ctpagseguro_camptix_load_addons' );
+	add_action( 'camptix_load_addons', 'ctpagseguro_camptix_load_addons' );
 }
 
 add_action( 'plugins_loaded', 'ctpagseguro_plugins_loaded' );
@@ -48,7 +48,7 @@ add_action( 'plugins_loaded', 'ctpagseguro_plugins_loaded' );
  * @return void
  */
 function ctpagseguro_camptix_load_addons() {
-    require_once 'includes/class-payment-method-pagseguro.php';
+	require_once 'includes/class-payment-method-pagseguro.php';
 }
 
 /**
@@ -60,15 +60,15 @@ function ctpagseguro_camptix_load_addons() {
  */
 function ctpagseguro_action_links( $links ) {
 
-    $settings = array(
-        'settings' => sprintf(
-            '<a href="%s">%s</a>',
-            admin_url( 'edit.php?post_type=tix_ticket&page=camptix_options&tix_section=payment' ),
-            __( 'Settings', 'ctpagseguro' )
-        )
-    );
+	$settings = array(
+		'settings' => sprintf(
+			'<a href="%s">%s</a>',
+			admin_url( 'edit.php?post_type=tix_ticket&page=camptix_options&tix_section=payment' ),
+			__( 'Settings', 'ctpagseguro' )
+		)
+	);
 
-    return array_merge( $settings, $links );
+	return array_merge( $settings, $links );
 }
 
 add_filter( 'plugin_action_links_' . plugin_basename( __FILE__ ), 'ctpagseguro_action_links' );
